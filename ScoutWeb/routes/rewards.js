dccac91d16d8138b022c9f9e03ee703da661f178
Thomas Fung <tfung@ualberta.ca>
2015-02-08 02:12:50 -0700

    awards are now queried into list

--- ScoutWeb/routes/rewards.js
+++ ScoutWeb/routes/rewards.js
@@ -3,7 +3,24 @@ var router = express.Router();
 var Parse = require('parse').Parse;
 
 router.get('/', function(req, res, next) {
-  res.render('rewards', { title: 'Scout' });
+  res.render('rewards', { title: 'Scout' , jumbotron: 'Rewards' });
+});
+
+router.get('/rewardslist', function(req, res, next) {
+  var RewardObj = Parse.Object.extend("Reward");
+  var query = new Parse.Query(RewardObj);
+
+  query.find({
+    success: function(list) {
+      var rewards = [];
+      /*
+      list.forEach(function (reward) {
+        console.log('\n DUMP:\n %j\n', reward);
+      });
+      */
+      res.json(list);
+    }
+  });
 });
 
 router.post('/', function (req, res) {
