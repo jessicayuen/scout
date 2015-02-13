84cda174395756f67f08b5a5642b8f7eaad3e84d
Jessica Yuen <jyuen@ualberta.ca>
2015-02-13 10:16:04 -0700

    Rename beaconDao to BeaconDao to remove project errors

--- ScoutMobile/app/src/main/java/scout/scoutmobile/dao/BeaconDao.java
+++ ScoutMobile/app/src/main/java/scout/scoutmobile/dao/BeaconDao.java
@@ -1,12 +1,8 @@
 package scout.scoutmobile.dao;
 
 import com.parse.FindCallback;
-import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import com.parse.ParseUser;
-
-import java.util.List;
 
 import scout.scoutmobile.constants.Consts;
 
