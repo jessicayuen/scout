2cf41dc5afb5e87b18ab68d8c29e084dc548a6ef
Jessica Yuen <jyuen@ualberta.ca>
2015-02-13 10:23:25 -0700

    Code cleanup - removing unused imports, code in wrong place, etc

--- ScoutMobile/app/src/main/java/scout/scoutmobile/ScoutAndroidApplication.java
+++ ScoutMobile/app/src/main/java/scout/scoutmobile/ScoutAndroidApplication.java
@@ -1,11 +1,8 @@
 package scout.scoutmobile;
 
 import android.app.Application;
-import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.widget.Toast;
 
 import com.estimote.sdk.BeaconManager;
