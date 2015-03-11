f6e6ebcb4f354e05d867dfdaa909c3a10daff6ff
jjhoy <jjhoy@ualberta.ca>
2015-03-11 01:47:19 -0600

    Changed heatmap page. Added temp point data and separated client code to Jquery.

---
+++ ScoutWeb/public/js/js-ejs/heatmap.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    // Populates heatmap with points
+    var initHeatmap = function (data) {
+            // Load floorplan
+            var src = "/img/floorplan.jpg";
+            $('#heatmapContainer').prepend('<img id="floorplan" src="' + src + '" />');
+
+            //create a heatmap instance
+            var heatmap = h337.create({
+              container:  $('#heatmapContainer')[0],
+              maxOpacity: .6,
+
+              radius: 30,
+              blur: .90,
+              // backgroundColor with alpha so you can see through it
+              backgroundColor: 'rgba(100, 100, 100, 0)'
+            });
+
+            heatmap.setData(data)
+            $('#heatmapContainer').hide().fadeIn(1000);
+    }
+
+    // Refreshes heatmap
+    var refreshHeatmap = function (callback) {
+        $.get('/heatmap/getheatmap')
+            .success(function (data) {
+                console.log(data);
+                $('#heatmapContainer').empty();
+                initHeatmap(data);
+            })
+        .error(function (xhr, textStatus, errorThrown) {
+            alert(xhr.responseText);
+        });
+
+        if (callback != null) callback();
+    }
+    // Refresh heatmap when page is loaded
+    $('#heatmapContainer').ready(refreshHeatmap);
+});
--- ScoutWeb/routes/heatmap.js
+++ ScoutWeb/routes/heatmap.js
@@ -4,16 +4,79 @@ var Parse = require('parse').Parse;
 
 
 router.get('/', function(req, res, next) {
-
     if (Parse.User.current()) {
-        res.render('heatmap', { title: 'Scout', filename: 'heatmap'});
+        res.render('heatmap', { title: 'Scout', filename: 'heatmap' } );
     } else {
         res.redirect('/');
     }
 });
 
 
-router.get('/heatmap', function(req, res, next) {
+router.get('/getheatmap', function(req, res, next) {
+
+    // Data contains max & min values and an array of point objects (TODO: replace with real data)
+    // For now generate some random data
+    var points = [];
+    var max = 0;
+    var width = 1281;
+    var height = 778; //TODO: Change based on image size
+    var len = 200;
+
+    while (len--) {
+      var val = Math.floor(Math.random()*100);
+      max = Math.max(max, val);
+      var point = {
+        x: Math.floor(Math.random()*width),
+        y: Math.floor(Math.random()*height),
+        value: val
+      };
+      points.push(point);
+    }
+    // heatmap data format
+    var data = { 
+      max: max, 
+      data: points 
+    };
+
+    res.json(data);
+
+
+    // TODO: Used for animated heatmap for we decide to implement
+    // setInterval(function () {
+    //   // Get a new day's data every 10 intervals
+    //   if (intervalCounter == 10){
+    //     intervalCounter = 0;
+    //     getAnotherDay(); 
+    //   } else {
+    //     intervalCounter++;
+    //   }
+
+    //   // Create new array for the next frame's points, remove old points, add new points, then update and push to map
+    //   var newData = [];
+    //   for(var j=0; j < data.data.length; j++) {
+    //     var point = data.data[j];
+
+    //     if(point.value >= 10) {
+    //       point.fresh = false;
+    //     }
+
+    //     // Fade in fresh points, fade out unfresh points
+    //     if(point.fresh) {
+    //       point.value = point.value + .8;
+    //     } else {
+    //       point.value = point.value - .1;
+    //     }
+            
+    //     if(point.value > 0) {
+    //       newData.push(data.data[j]);
+    //     }
+    //   }
+    //   data.data = newData;
+          
+    //   heatmapLayer.setData(data);
+    // }, 100);
+
+
 
 });
 
