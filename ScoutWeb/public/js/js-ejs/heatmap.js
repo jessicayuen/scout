e2fa11c0fc3c98c5ec75cd10f284dd955e4bcce8
jjhoy <jjhoy@ualberta.ca>
2015-04-01 19:28:09 -0600

    Added beacon API and cleaned heatmap code.

--- ScoutWeb/public/js/js-ejs/heatmap.js
+++ ScoutWeb/public/js/js-ejs/heatmap.js
@@ -1,45 +1,4 @@
 $(document).ready(function(){
-    
-    var intervalJsonData;
-    var intervalRecordsJsonData;
-
-    // Populates heatmap with points
-    var initHeatmap = function (data) {
-            // Load floorplan
-            var src = "/img/floorplan.jpg";
-            $('#heatmapContainer').prepend('<img id="floorplan" src="' + src + '" />');
-
-            $("<img />")
-                .attr("src", src)
-                .load(function() {
-                    $("#heatmapContainer").height(this.height);
-                    $("#heatmapContainer").width(this.width);
-                                                                //create a heatmap instance
-                        var heatmap = h337.create({
-                          container:  $('#heatmapContainer')[0],
-                          maxOpacity: .6,
-
-                          radius: 30,
-                          blur: .90,
-                          // backgroundColor with alpha so you can see through it
-                          backgroundColor: 'rgba(100, 100, 100, 0)'
-                        });
-
-                        heatmap.setData(data)
-                        $('#heatmapContainer').hide().fadeIn(1000);
-            });
-    }
-
-    function getheatmapAjax() {
-        return $.get('/heatmap/getheatmap')
-        .success(function (data) {
-            intervalJsonData = data;
-            console.log(data);
-        })
-        .error(function (xhr, textStatus, errorThrown) {
-            alert(xhr.responseText);
-        });
-    }
 
     function getIntervalJSONAjax() {
         return $.get('/heatmap/retrieveIntervalJSON')
@@ -54,7 +13,6 @@ $(document).ready(function(){
     function getIntervalRecordsJSONAjax() {
         return $.get('/heatmap/retrieveIntervalRecordsJSON')
         .success(function (data) {
-            intervalRecordsJsonData = data
             console.log(data);
         })
         .error(function (xhr, textStatus, errorThrown) {
@@ -62,18 +20,14 @@ $(document).ready(function(){
         });
     }
 
-    // Refreshes heatmap
-    var refreshHeatmap = function (callback) {
-        $.when(getheatmapAjax(), getIntervalJSONAjax()).done(function(a1, a2){
-            // the code here will be executed when all four ajax requests resolve.
-            // a1, a2, a3 are lists of length 3 containing the response text,
-            // status, and jqXHR object for each of the 3 ajax calls respectively.
-            $('#heatmapContainer').empty();
-            // initHeatmap(intervalJsonData);
+    function getBeaconsJSONAjax() {
+        return $.get('/heatmap/retrieveBeaconsJSON')
+        .success(function (data) {
+            console.log(data);
+        })
+        .error(function (xhr, textStatus, errorThrown) {
+            alert(xhr.responseText);
         });
-        if (callback != null) callback();
     }
 
-    // Refresh heatmap when page is loaded
-    $('#heatmapContainer').ready(refreshHeatmap);
 });
--- ScoutWeb/routes/heatmap.js
+++ ScoutWeb/routes/heatmap.js
@@ -23,7 +23,7 @@ router.get('/retrieveIntervalJSON', function(req, res, next) {
     parseHandler.retrieveIntervalJSON(successCb, errorCb, {'user': Parse.User.current()});
 });
 
-// routing to test retrieveIntervalRecordsJSON
+// routing to retrieveIntervalRecordsJSON
 router.get('/retrieveIntervalRecordsJSON', function(req, res, next) {
     var successCb = function (intervalRecords) {
         res.json(intervalRecords); // TODO: need to populate
@@ -35,7 +35,20 @@ router.get('/retrieveIntervalRecordsJSON', function(req, res, next) {
     parseHandler.retrieveIntervalRecordsJSON(successCb, errorCb, {'user': Parse.User.current()});
 });
 
-router.get('/getheatmap', function(req, res, next) {
+// routing to retrieveBeaconsJson
+router.get('/retrieveBeaconsJSON', function(req, res, next) {
+    var successCb = function (beacons) {
+        res.json(beacons);
+    }
+    var errorCb = function (beacons, error) {
+        console.log(error);
+        res.status(400).send('ERROR: Cannot retrieve beacons');
+    }
+    parseHandler.retrieveBeaconsJSON(successCb, errorCb, {'user': Parse.User.current()});
+});
+
+// routing for testing data
+router.get('/getTestHeatmap', function(req, res, next) {
 
     // Data contains max & min values and an array of point objects (TODO: replace with real data)
     // For now generate some random data
@@ -82,44 +95,6 @@ router.get('/getheatmap', function(req, res, next) {
     }
     console.log(data);
     res.json(data);
-
-
-    // TODO: Used for animated heatmap for we decide to implement
-    // setInterval(function () {
-    //   // Get a new day's data every 10 intervals
-    //   if (intervalCounter == 10){
-    //     intervalCounter = 0;
-    //     getAnotherDay(); 
-    //   } else {
-    //     intervalCounter++;
-    //   }
-
-    //   // Create new array for the next frame's points, remove old points, add new points, then update and push to map
-    //   var newData = [];
-    //   for(var j=0; j < data.data.length; j++) {
-    //     var point = data.data[j];
-
-    //     if(point.value >= 10) {
-    //       point.fresh = false;
-    //     }
-
-    //     // Fade in fresh points, fade out unfresh points
-    //     if(point.fresh) {
-    //       point.value = point.value + .8;
-    //     } else {
-    //       point.value = point.value - .1;
-    //     }
-            
-    //     if(point.value > 0) {
-    //       newData.push(data.data[j]);
-    //     }
-    //   }
-    //   data.data = newData;
-          
-    //   heatmapLayer.setData(data);
-    // }, 100);
-
-
 
 });
 
